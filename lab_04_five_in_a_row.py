f923e3c3f8c66649b93cd77d3c9351b030f7af27
Erishanom <33721329+Erishanom@users.noreply.github.com>
2017-11-16 16:08:51 +0100

    Update lab_04_five_in_a_row.py

--- lab_04_five_in_a_row.py
+++ lab_04_five_in_a_row.py
@@ -1,4 +1,4 @@
-def print_game(game):
+def print_game(game, next_player):
     # Prints the game to the console.
 
     print(" y")
@@ -9,7 +9,7 @@ def print_game(game):
         print(str(y)+"|", end="")
 
         for x in range(game['width']):
-            print(get_cell_value(game, x, y), end="")
+            print(get_cell_value(game, x, y, next_player), end="")
         print()
 
         y -= 1
@@ -23,12 +23,16 @@ def print_game(game):
         print(x, end="")
     print(" ")
 
-def get_cell_value(game, x, y):
-    all_cell_value = game.get('x-moves')+ game.get('o-moves')
+def get_cell_value(game, x, y, next_player):
+    all_cell_value = game.get('x-moves')
     for i in all_cell_value:
         if i.get('x') == x and i.get('y') == y:
-            return i.get('player')
-    return ' '
+            return 'X'
+    
+    all_cell_value = game.get('o-moves')
+    for i in all_cell_value:
+        if i.get('x') == x and i.get('y') == y:
+            return 'O'
 
     # Returns 'X' if a cross has been placed in the cell with the given coordinates.
     # Returns 'O' if a circle has been placed in the cell with the given coordinates.
@@ -41,7 +45,7 @@ def make_move(game, x, y, next_player):
         game.get('x-moves').append(newest_move)
     elif next_player =='O':
         newest_move = {'x': x, 'y': y}
-        game.get('x-moves').append(newest_move)
+        game.get('o-moves').append(newest_move)
         
         
 
@@ -57,7 +61,7 @@ def does_cell_exist(game, x, y):
     # Returns False otherwise.
 
 def is_cell_free(game, x, y):
-    all_cell_taken = game.get('moves')
+    all_cell_taken = game.get('x-moves') + game.get('o-moves')
 
     for i in all_cell_taken:
         if i.get('x') == x and i.get('y') == y:
@@ -77,28 +81,88 @@ def get_next_players_turn(game, i):
     # Returns 'X' if a cross should be placed on the board next.
     # Returns 'O' if a circle should be placed on the board next.
 
+def check_horizontal(game, next_player):
+    if next_player == 'X':
+         check_if_winner = game.get('x-moves')
+    else:
+         check_if_winner = game.get('o-moves')
+    for y in range (0,9):
+        for x in range (0,5):
+            winner_dict =[
+                {'x': x, 'y': y},
+                {'x': x+1, 'y': y},
+                {'x': x+2, 'y': y},
+                {'x': x+3, 'y': y},
+                {'x': x+4, 'y': y}]
+            if winner_dict in check_if_winner:
+                return next_player
+                
+    return False
 
-def get_winner(game):
-    check_if_x_is_winner = game.get('moves')
-    t=0
-    winner_dict =  [
-        {'x': 0, 'y': 0,} 
-        {'x': 1, 'y': 0,}
-        {'x': 2, 'y': 0,}
-        {'x': 3, 'y': 0,}
-        {'x': 4, 'y': 0,}
-        ]
-    for key in winner_dict.keys():
-        if (not key in check_if_x_is_winner) or (not winner_dict[key] == check_if_x_is_winner[key]):
-            for i in len(winner_dict):
-                i['x'] +=1
-                t+=1
-                if t >=5:
-                    return 'X'
-         else:
-             if 
+ 
+def check_vertical(game, next_player):
+    if next_player == 'X':
+         check_if_winner = game.get('x-moves')
+    else:
+         check_if_winner = game.get('o-moves')
+    for x in range (0,9):
+        for y in range (0,5):
+            winner_dict =  [
+                {'x': x, 'y': y},
+                {'x': x, 'y': y+1},
+                {'x': x, 'y': y+2},
+                {'x': x, 'y': y+3},
+                {'x': x, 'y': y+4}]
+            if winner_dict in check_if_winner:
+                return next_player
+    return False
 
+def check_diagonal_up(game, next_player):
+    if next_player == 'X':
+         check_if_winner = game.get('x-moves')
+    else:
+         check_if_winner = game.get('o-moves')
+    for y in range (0,5):
+        for x in range (0,5):
+            winner_dict =  [
+                {'x': x, 'y': y},
+                {'x': x+1, 'y': y+1},
+                {'x': x+2, 'y': y+2},
+                {'x': x+3, 'y': y+3},
+                {'x': x+4, 'y': y+4}]
+            if winner_dict in check_if_winner:
+                return True
+    return False     
 
+def check_diagonal_down(game, next_player):
+    if next_player == 'X':
+         check_if_winner = game.get('x-moves')
+    else:
+         check_if_winner = game.get('o-moves')
+    check_if_winner.sort()
+    for y in range (0,5):
+        for x in range (5,9):
+            winner_dict =  [
+                {'x': x, 'y': y},
+                {'x': x-1, 'y': y+1},
+                {'x': x-2, 'y': y+2},
+                {'x': x-3, 'y': y+3},
+                {'x': x-4, 'y': y+4}]
+            if winner_dict in check_if_winner:
+                return True
+    return False
+
+def get_winner(game, next_player):
+    if check_horizontal(game, next_player) == True:
+        return True
+    elif check_vertical(game, next_player) == True:
+        return True
+    elif check_diagonal_up(game, next_player) == True:
+        return True
+    elif check_diagonal_down(game, next_player) == True:
+        return True
+    else:
+        return ' '
     # Returns 'X' if 5 crosses in a row is found in the game.
     # Returns 'O' if 5 circles in a row is found in the game.
     # Returns ' ' otherwise.
